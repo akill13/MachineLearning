69844abc42869eed4c5cc37ba3906c0f2fff405d
Abdul Halimi <abdulhalimi13@yahoo.com>
2019-08-25 21:07:00 -0400

    Initial set up for binary classification system

--- MachineLearning/ViewController.swift
+++ MachineLearning/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
-
+import CoreML
+import Vision
 class ViewController: UIViewController {
-
+    lazy var classificationRequest: VNCoreMLRequest = {
+        do {
+            let healthySnacks = HealthySnacks()
+            let visionModel = try VNCoreMLModel(for: healthySnacks.model)
+            let request = VNCoreMLRequest(model: visionModel, completionHandler: {
+                [weak self] request, error in
+                print("Request is finished", request.results as Any)
+            })
+            request.imageCropAndScaleOption = .centerCrop
+            return request
+        } catch {
+            fatalError("Failed to create VNCoreMLModel: \(error)")
+        }
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func classify(image: UIImage) {
+        
     }
 
 
